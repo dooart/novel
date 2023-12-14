d6379622bb36681bde7e3f24a7e17a01f2eeeb5e
thiago <371426+dooart@users.noreply.github.com>
2023-12-13 22:58:27 -0300

    Fix tailwind warning about nesting

--- packages/core/tailwind.config.js
+++ packages/core/tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
   // prefix ui lib classes to avoid conflicting with the app
   ...sharedConfig,
   prefix: "novel-",
-  plugins:[
-    ...sharedConfig.plugins,
+  plugins: [
     scopedPreflightStyles({
-      cssSelector: '.novel-twp', // or .tailwind-preflight or even [data-twp=true] - any valid CSS selector of your choice
-      mode: 'matched only', // it's the default
-  }),
-  ]
+      cssSelector: ".novel-twp", // or .tailwind-preflight or even [data-twp=true] - any valid CSS selector of your choice
+      mode: "matched only", // it's the default
+    }),
+    ...sharedConfig.plugins,
+  ],
 };
